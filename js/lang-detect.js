f39b2193f520362b59f626d42e0bffc69833c4b4
Nachoco <ignaciocozza@gmail.com>
2019-10-04 10:56:48 -0300

    Index english. Wording in ES page. Redirect via JS depending on browser lang

---
+++ js/lang-detect.js
@@ -0,0 +1,60 @@
+/*
+ Detects the browser language
+ */
+var userLang = navigator.language || navigator.userLanguage;
+
+/*
+ test if ou cookie is set
+ */
+if(getCookie("lang")){
+  //   it is set, so no redirect
+}
+else{
+  // The cookie is not set, so set it, and redirect
+  langstring=userLang.split('-')[0];
+  lang = "lang="+langstring;
+  document.cookie=lang;
+  redirect(langstring);
+}
+
+/*
+ Returns the given cookie or false if not existing
+ */
+function getCookie(name){
+  var re = new RegExp(name + "=([^;]+)");
+  var value = re.exec(document.cookie);
+  return (value != null) ? unescape(value[1]) : null;
+}
+
+/*
+ Function used to clean our language cookie
+ */
+function clearCookie(){
+  var mydate = new Date();
+  mydate.setTime(mydate.getTime() - 1);
+  document.cookie = "lang=; expires=" + mydate.toGMTString();
+}
+
+/*
+ redirects to the desired URLs
+ Only the first two letters of the ISO country code are used
+ */
+function redirect(actuallang){
+  var url ='/';
+  switch(actuallang){
+	case 'en': case 'ru': case 'fr': case 'de': default:
+      url ='https://virtualdot.info';
+      break;
+    case 'es':
+      url ='https://virtualdot.info/index-es.html';
+      break;
+  }
+  console.log("old loc");
+  console.log(window.location.href);
+  console.log("new loc");
+  console.log(url);
+  console.log(url == window.location);
+  if (url != window.location) {
+  	//  window.location.replace(url);
+  }
+}
